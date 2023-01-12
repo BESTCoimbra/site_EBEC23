284f59c5c0d148e5ed781e4d904a67fe7b47927b
Paulo Meira <75759802+Palmeira01@users.noreply.github.com>
2023-01-12 00:29:20 +0000

    creation of the FAQ section
    Ainda falta corrigir algumas coisas

--- js/script.js
+++ js/script.js
@@ -7,3 +7,21 @@
 		loader.classList.add("disappear");
 	}
 })();
+
+function openResponse(id) {
+	var response = document.querySelector('.response-'+id);
+	var question = document.querySelector('.question-'+id);
+	var seta = document.querySelector('.seta-'+id);
+	if (response.style.display === 'block'){
+		response.style.display = "none";
+		question.style.backgroundColor = "white"
+		seta.style.boxShadow = "2px -2px 0 0.5px var(--orange-visualidentityorange) inset"
+		seta.style.transform = "rotate(-45deg)"
+	}
+	else {
+		response.style.display = "block"
+		question.style.backgroundColor = "var(--orange-visualidentityorange)"
+		seta.style.boxShadow = "2px -2px 0 0.5px white inset"
+		seta.style.transform = "rotate(135deg)"
+	}
+}
