eed5e47a0890625e8f60337b61b60c3546b92340
Paulo Meira <75759802+Palmeira01@users.noreply.github.com>
2023-01-13 03:29:13 +0000

    updates FAQ
    Melhorias no modo como as respostas são reveladas

--- js/script.js
+++ js/script.js
@@ -13,7 +13,8 @@ function desativateOpenResponses(id) {
 	var question = document.querySelector('.question-'+id);
 	var seta = document.querySelector('.seta-'+id);
 
-	response.style.display = "none";
+	response.style.maxHeight = "0";
+	response.style.padding = "0 35px"
 	question.style.backgroundColor = "white"
 	seta.style.boxShadow = "2px -2px 0 0.5px var(--orange-visualidentityorange) inset"
 	seta.style.transform = "rotate(-45deg)"
@@ -28,14 +29,16 @@ function openResponse(id) {
 		if (id != i) {desativateOpenResponses(i)}
 	}
 
-	if (response.style.display === 'block'){
-		response.style.display = "none";
+	if (response.style.maxHeight === '300px'){
+		response.style.maxHeight = "0";
+		response.style.padding = "0 35px"
 		question.style.backgroundColor = "white"
 		seta.style.boxShadow = "2px -2px 0 0.5px var(--orange-visualidentityorange) inset"
 		seta.style.transform = "rotate(-45deg)"
 	}
 	else {
-		response.style.display = "block"
+		response.style.maxHeight = "300px"
+		response.style.padding = "20px 35px"
 		question.style.backgroundColor = "var(--orange-visualidentityorange)"
 		seta.style.boxShadow = "2px -2px 0 0.5px white inset"
 		seta.style.transform = "rotate(135deg)"
