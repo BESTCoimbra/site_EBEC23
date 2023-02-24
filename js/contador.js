299bf799eace699ef06ee48849c18610ea56be48
Paulo Meira <75759802+Palmeira01@users.noreply.github.com>
2023-02-24 19:58:47 +0000

    Optimização do site

--- js/contador.js
+++ js/contador.js
@@ -1,4 +1,4 @@
-var deadline = new Date("Feb 22, 2023 23:59:59").getTime();
+var deadline = new Date("Feb 24, 2023 23:59:59").getTime();
 var x = setInterval(function() {
 var now = new Date().getTime();
 var t = deadline - now;
@@ -7,10 +7,18 @@ var hours = Math.floor((t%(1000 * 60 * 60 * 24))/(1000 * 60 * 60));
 var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
 var seconds = Math.floor((t % (1000 * 60)) / 1000);
 
-document.querySelector(".demo-days-p").innerHTML = days;
-document.querySelector(".demo-hours-p").innerHTML = hours;
-document.querySelector(".demo-minutes-p").innerHTML = minutes;
-document.querySelector(".demo-seconds-p").innerHTML = seconds;
+if(days<0){
+    document.querySelector(".demo-days-p").innerHTML = 0;
+    document.querySelector(".demo-hours-p").innerHTML = 0;
+    document.querySelector(".demo-minutes-p").innerHTML = 0;
+    document.querySelector(".demo-seconds-p").innerHTML = 0;
+}
+else {
+    document.querySelector(".demo-days-p").innerHTML = days;
+    document.querySelector(".demo-hours-p").innerHTML = hours;
+    document.querySelector(".demo-minutes-p").innerHTML = minutes;
+    document.querySelector(".demo-seconds-p").innerHTML = seconds;
+}
 
 if (t < 0) {
     clearInterval(x);
